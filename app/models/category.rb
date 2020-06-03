b04474495dcfea6abc5daa8a615fbc488f1838cf
Samantha Lee <samanthaanne.lee28@gmail.com>
2020-06-03 22:13:43 +0800

    Edited models to help in seeding.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  has_many :pets
+
+  validates :animal_type, presence: true
+
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,10 @@
 class Pet < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price_per_hour, presence: true
+  validates :user_id, presence: true
+  validates :categories_id, presence: true
+
 end
