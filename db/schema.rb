a67dce13c1ebc4d2f933a021194c77cb4fbc2fdf
Samantha Lee <samanthaanne.lee28@gmail.com>
2020-06-03 20:32:42 +0800

    Updated Sign Up fields.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_154603) do
+ActiveRecord::Schema.define(version: 2020_06_03_121714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,8 @@ ActiveRecord::Schema.define(version: 2020_06_02_154603) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "regions_id"
+    t.string "name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["regions_id"], name: "index_users_on_regions_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
