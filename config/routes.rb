42ea07d3d53fb19fd89faa280e50917829856f31
Kahhow Lee <satoshitzum@gmail.com>
2020-06-04 20:38:36 +0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pets
-  resources :users do
-    resources :bookings, only: [:new, :create, :edit, :update]
-  end
+  resources :bookings, only: [:new, :create, :edit, :update]
 
-  resources :bookings do
+  resources :bookings, only: [:new, :create]  do
     resources :reviews, only: [:new, :create]
   end
 
