05fe5a73213e2e565ce74ff25ac7d5ffb8d71721
Kahhow Lee <satoshitzum@gmail.com>
2020-06-04 20:11:24 +0800

    removed stray comma

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,7 +5,7 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true, date: { after_or_equal_to: :start_date}
   validates :start_time, presence: true
-  validates :end_time, presence: true,
+  validates :end_time, presence: true
   validates :pet_id, presence: true
 
 end
