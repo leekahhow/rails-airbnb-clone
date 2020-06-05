3f96ba3bc8628ced759c85976802bcbf30ac9c0c
Samantha Lee <samanthaanne.lee28@gmail.com>
2020-06-05 13:16:59 +0800

    Booking Form is viewable on the website page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,17 +1,27 @@
 class BookingsController < ApplicationController
 
   def new
+    # Initiate new Booking Instance
     @booking = Booking.new
   end
 
   def create
+    # Pass booking details to new booking instance
     @booking = Booking.new(booking_params)
+    # Pass User id to new booking instance
+    @user = User.find(params[:user_id])
+    # Chaning the status = valid of new booking instance
+    @booking.status = "valid"
+    # If/Else Save
     if @booking.save
       redirect_to bookings_path
     else
       render :new
     end
   end
+
+  #Bookings have an :id, :user_id, :start_date, :hours, :status, :ped_id
+
 
   def cancel
     @booking = booking.find(params[:id])
