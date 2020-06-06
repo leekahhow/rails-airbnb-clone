e5ad82e00602d5840a2a443204034f6aa5768378
Kahhow Lee <satoshitzum@gmail.com>
2020-06-06 13:21:44 +0800

    fixed cloudinary

---
+++ db/migrate/20200606051306_remove_photo_field_from_pets_table.rb
@@ -0,0 +1,5 @@
+class RemovePhotoFieldFromPetsTable < ActiveRecord::Migration[6.0]
+  def change
+    remove_column :pets, :photo
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_06_002753) do
+ActiveRecord::Schema.define(version: 2020_06_06_051306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,6 @@ ActiveRecord::Schema.define(version: 2020_06_06_002753) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id"
-    t.string "photo"
     t.index ["category_id"], name: "index_pets_on_category_id"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
