3348c0f9363c9802e40aa4c560dc1e3817b9d6cd
Kahhow Lee <satoshitzum@gmail.com>
2020-06-06 16:10:50 +0800

    updated styling for all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'reviews/new'
   get 'reviews/create'
-  devise_for :users
   root to: 'pages#home'
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pets do
     resources :bookings, only: [:new, :create] # :edit, :update
