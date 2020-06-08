a224677b8fadef27638f290188ea0d319159a33e
Kahhow Lee <satoshitzum@gmail.com>
2020-06-08 16:23:43 +0800

    resolved booking image display issues and fixed reviews page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,21 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    @booking = Booking.find(params[:booking_id])
   end
 
   def create
     @review = Review.new(review_params)
-      if @review.save
-        redirect_to reviews_path
+    @booking = Booking.find(params[:booking_id])
+    @review.reviewable = @booking
+    if @review.save
+      redirect_to user_bookings_path(current_user.id)
     else
       render :new
     end
   end
 
-    private
+  private
 
   def review_params
     params.require(:review).permit(:description, :rating, :reviewable_id)
