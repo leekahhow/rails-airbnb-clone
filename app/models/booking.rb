92b041ba85627b0150dad0bc57392a84dd71c256
Kahhow Lee <satoshitzum@gmail.com>
2020-06-09 13:27:44 +0800

    updated reviews for pets show

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :pet
   belongs_to :user
+  has_many :reviews, as: :reviewable
 
   validates :start_date, presence: true
   validates :hours, presence: true
