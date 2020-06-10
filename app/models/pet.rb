7a48a170a386cf07a3c4e0c79cf31047f0424e13
Samantha Lee <samanthaanne.lee28@gmail.com>
2020-06-11 00:09:52 +0800

    Changed Search to focus on Category :animal_type

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -10,14 +10,4 @@ class Pet < ApplicationRecord
   validates :description, presence: true
   validates :price_per_hour, presence: true
 
-  include PgSearch::Model
-  pg_search_scope :search_by_name,
-    against: :name, #what to search
-    using: {
-      tsearch: { prefix: true }
-    }
-
-  include PgSearch::Model
-    multisearchable against: :name
-
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,7 +1,3 @@
 class Region < ApplicationRecord
   has_many :users, dependent: :destroy
-
-  include PgSearch::Model
-  multisearchable against: :name
-
 end
