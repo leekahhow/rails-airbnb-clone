fe1b665089a33c8fe24ae5fa97493ae7cb066c7f
Kahhow Lee <satoshitzum@gmail.com>
2020-06-11 22:54:25 +0800

    show reviews for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @pet = @user.pets
-    raise
-    @reviews = Review.where(:reviewable == @current_user)
+    @reviews = current_user.reviews
   end
 end
