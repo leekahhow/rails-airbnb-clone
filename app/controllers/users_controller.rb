4703d547009e152d1ca4dffc5b063e64924de490
Kahhow Lee <satoshitzum@gmail.com>
2020-06-11 22:45:47 +0800

    solved booking if-else

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @pet = @user.pets
+    raise
     @reviews = Review.where(:reviewable == @current_user)
   end
 end
