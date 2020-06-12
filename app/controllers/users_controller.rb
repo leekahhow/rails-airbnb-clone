97537d4e2467f28b22f0bacbeb3743a534affc71
Kahhow Lee <satoshitzum@gmail.com>
2020-06-12 22:16:33 +0800

    updated user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @pet = @user.pets
+    #list of reviews tagged under the user
     @reviews = Review.where(:reviewable => @user)
+    #list of users
+    users_id = @reviews.pluck(:reviewable_id)
+    @users_who_left_reviews = User.where(id: users_id)
   end
 end
